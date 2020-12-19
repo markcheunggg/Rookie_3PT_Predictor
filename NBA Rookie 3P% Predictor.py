d3a761ba0a23232c0aaa50a4dc45d3b55613756c
markcheunggg <66694256+markcheunggg@users.noreply.github.com>
2020-12-18 16:46:20 -0800

    Add files via upload

--- NBA Rookie 3P% Predictor.py
+++ NBA Rookie 3P% Predictor.py
@@ -1,9 +1,11 @@
 import pandas as pd
+import numpy as np
 from sklearn.impute import SimpleImputer
 from xgboost import XGBRegressor
 from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.cluster import KMeans
+from sklearn.metrics.pairwise import euclidean_distances
 
 """
 Rookie 3P% Improvement Predictor.
@@ -15,14 +17,11 @@ similar college and rookie shooting numbers to that player.
 """
 
 # Import and read the data.
-NBA_shooting_stats = pd.read_csv("3PT Prediction Model Training Stats.csv")  #Shooting stats from perimeter players drafted from 2010-2018.
-rookies2019_20_stats = pd.read_csv("rookies.csv")  #Shooting stats from rookies drafted in 2019.
+NBA_shooting_stats = pd.read_csv("3PT Prediction Model Training Stats.csv")  # Shooting stats from perimeter players drafted between 2010-2018.
+rookies2019_20_stats = pd.read_csv("rookies.csv")  # Shooting stats from rookies drafted in 2019.
 
 # The stat we are predicting for: 3P% improvement from first to second NBA season.
 y = NBA_shooting_stats['3P% Improve']
-
-# Fill in any blank entries with the mean value
-y.fillna(y.mean(), inplace=True)
 
 # The features used to predict 3P% improvement for the Random Forest model:
 rf_predictors = ['Draft Position', 'Age', '3P% 1st Season', 'FT% 1st Season', '3P% College Improve',
@@ -40,7 +39,8 @@ similarity_X = NBA_shooting_stats[similarity_stat_predictors]
 rf_X = NBA_shooting_stats[rf_predictors]
 xgb_X = NBA_shooting_stats[xgb_predictors]
 
-# Fill in any blank entries with the mean values.
+# Fill in any blank entries with the respective column's mean values
+# and normalize the data so that every feature is scaled equally.
 imputer = SimpleImputer(strategy="mean")
 
 imputed_rf_x = pd.DataFrame(imputer.fit_transform(rf_X))
@@ -68,8 +68,8 @@ def xgb_error():
 def rf_error():
 
     """
-   Prints out the MAE and Median Absolute Error of the Random Forest Model using cross validation.
-   """
+    Prints out the MAE and Median Absolute Error of the Random Forest Model using cross validation.
+    """
     mean_score = -1 * cross_val_score(rf_model, imputed_rf_x, y, cv=5, scoring='neg_mean_absolute_error')
     median_score = -1 * cross_val_score(rf_model, imputed_rf_x, y, cv=5, scoring='neg_median_absolute_error')
     print("RF cross-val mean absolute error:", mean_score.mean())
@@ -109,9 +109,9 @@ def xgb_predict_curr(name):
     """
     A function for predicting a current 2019-20 rookie's 3P% shooting improvement from their rookie season to their
     sophomore season using a XGB Boost model.
-   :param name: A string of the rookie's name.
-   Prints out the predicted increase/decrease in 3P%
-   """
+    :param name: A string of the rookie's name.
+    Prints out the predicted increase/decrease in 3P%
+    """
 
     # Get the row of shooting stats from our rookie shooting stats dataframe for the player we are predicting.
     specific_rookie_stats = rookies2019_20_stats.loc[rookies2019_20_stats["Name"] == name, :]
@@ -132,6 +132,48 @@ def xgb_predict_curr(name):
         print("The model predicts that " + name + "'s 3P% will increase by " + str(rookie_prediction)[1:-1] + "% next season.")
     else:
         print("The model predicts that " + name + "'s 3P% will decrease by " + str(rookie_prediction)[1:-1] + "% next season.")
+
+def cluster_distances(clusters, data, results, num):
+    """
+    This function is used to find
+    1) the average distance between clusters.
+    2) the average of the average distances between each cluster's nodes and its center.
+
+    This is to find the optimal number of clusters to use when finding similar shooters. An optimal cluster should
+    maximize the average distance between clusters (1) and minimize the average of the average distances between each
+    cluster's nodes and its center (2).
+
+    :param clusters: The unfitted clusters
+    :param data: The features used for classifying.
+    :param results: The labels for each data point (which cluster each player is in).
+    :param num: The number of clusters being used.
+
+    Prints out the average distance between clusters (1) and the average of the average distances between
+    each cluster's nodes and its center (2).
+    """
+
+    # Get the distance in between distinct clusters.
+    dists_between_clust = euclidean_distances(clusters.fit(data).cluster_centers_)
+    tri_dists = dists_between_clust[np.triu_indices(num, 1)]
+
+    # Find the average distance in between clusters.
+    avg_dist_between_clust = tri_dists.mean()
+
+    # Get the squared distance from nodes to their cluster's center.
+    inter_clust_dist = clusters.transform(data)**2
+
+    # Put the squared distances into a dataframe to better visualize results and so we can group by cluster.
+    df = pd.DataFrame(inter_clust_dist.sum(axis=1).round(2), columns=['Squared Dist'])
+    df["Cluster"] = results
+
+    # Average of the average squared distances for each cluster.
+    average_dist_from_center = np.mean(df.groupby(["Cluster"]).mean()["Squared Dist"])
+
+    # Print out the average distance in between clusters (Want to maximize).
+    print("Avg Dist between clusters: " + str(avg_dist_between_clust))
+
+    # Print out the average squared distances from nodes to their cluster's center (Want to minimize).
+    print("Avg Dist from nodes to center: " + str(average_dist_from_center))
 
 
 def similarities(name):
@@ -161,7 +203,8 @@ def similarities(name):
     similarity_stats["Name"] = NBA_shooting_stats["Name"]
 
     # Set up our K-means clusters.
-    km = KMeans(n_clusters=10, max_iter=300, random_state=2)
+    num_clusters = 6
+    km = KMeans(n_clusters=num_clusters, max_iter=300, random_state=2)
 
     # Add our rookie to the dataframe of all our past rookies.
     with_new_player = similarity_stats.append(player_stats)
@@ -183,9 +226,9 @@ def similarities(name):
         new_list.append(str(x)[2:-2])
     print(new_list)
 
+
 # User Input
 player_name = input("Enter a NBA player's name: ")
-
 if player_name in rookies2019_20_stats.Name.values:
     # Predicts a Rookie's Shooting improvements using the a Random Forests Model.
     rf_predict_curr(player_name)
